54509825db63877c199bd8705c3133540fa54fae
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-10-14 07:15:55 +0100

    finishing my react investment calc

--- 003 Essentials Practice Project/Starting-project/src/App.jsx
+++ 003 Essentials Practice Project/Starting-project/src/App.jsx
@@ -6,11 +6,13 @@ import Results from "./components/Results.jsx";
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
+    initialInvestment: 15000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
+
+  const inputIsValid = userInput.duration >= 1;
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
@@ -26,7 +28,10 @@ function App() {
     <>
       <Header />;
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than zero.</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
