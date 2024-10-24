d463d7defa7fc5e13cbfd223456106d37bf6f867
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-10-24 18:23:09 +0100

    explored the concept od side effects useEffect

--- 009 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ 009 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,4 +1,22 @@
+import { useEffect } from "react";
+
+import ProgressBar from "./ProgressBar.jsx";
+
+const TIMER = 3000;
+
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("TIMER IS SET");
+      onConfirm();
+    }, TIMER);
+
+    return () => {
+      console.log("TIMER IS CLEARED OUT");
+      clearTimeout(timer);
+    };
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +29,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
