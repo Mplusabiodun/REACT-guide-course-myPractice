db33c1138ede4a6d7b94a4108320d833a3f6a940
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-11-06 13:06:20 +0100

    http request

--- 15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ 15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -1,10 +1,57 @@
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
+import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
+import { fetchAvialablePlaces } from "../http.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState();
+
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const places = await fetchAvialablePlaces();
+
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvailablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
+      } catch (error) {
+        setError({
+          message: error.message || "Could not fetch data, an error occured",
+        });
+        setIsFetching(false);
+      }
+    }
+    fetchPlaces();
+  }, []);
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/places")
+  //     .then((response) => {
+  //       return response.json();
+  //     })
+  //     .then((responseData) => {
+  //       setAvailablePlaces(responseData.places);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <Error title="An error occured" message={error.message} />;
+  }
   return (
     <Places
       title="Available Places"
-      places={[]}
+      isLoading={isFetching}
+      loadingText="Fetching place data...."
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
