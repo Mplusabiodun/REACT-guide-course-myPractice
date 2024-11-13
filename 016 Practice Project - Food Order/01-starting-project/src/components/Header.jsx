5095a94538977cba78bc65836ae4051d92ad0382
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-11-13 11:06:33 +0100

    fetching meals data

---
+++ 016 Practice Project - Food Order/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import reactFood from "../assets/logo.jpg";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={reactFood} alt="react-food" />
+        <h1>REACTFOOD</h1>
+      </div>
+
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
