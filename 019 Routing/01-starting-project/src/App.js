f81edafdeccc759cd92883445ef3c464203ab927
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-11-27 15:44:24 +0100

    Exploring react router

--- 019 Routing/01-starting-project/src/App.js
+++ 019 Routing/01-starting-project/src/App.js
@@ -1,5 +1,36 @@
+import {
+  createBrowserRouter,
+  // createRoutesFromElements,
+  // Route,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from "./pages/Home.js";
+import ProductsPage from "./pages/Products.js";
+import RootLayout from "./pages/Root.js";
+
+// const routeDefinition = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinition);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/products", element: <ProductsPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
