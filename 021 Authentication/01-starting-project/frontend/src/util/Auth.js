ddef763f69b2d388ef5f79e398381770c3a9c793
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-12-03 17:07:30 +0100

    Explored the concept of Authentication

--- 021 Authentication/01-starting-project/frontend/src/util/Auth.js
+++ 021 Authentication/01-starting-project/frontend/src/util/Auth.js
@@ -1,6 +1,39 @@
+import { redirect } from "react-router-dom";
+
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+}
+
 export function GetAuthToken() {
   const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
   return token;
+}
+
+export function tokenLoader() {
+  return GetAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = GetAuthToken();
+
+  if (!token) {
+    return redirect("/auth");
+  }
 }
 
 // import { redirect } from 'react-router-dom';
