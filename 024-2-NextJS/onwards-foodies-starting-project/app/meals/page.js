7f6114983d9e7b3ed4f45dcbd50600844dd41000
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-12-14 08:35:09 +0100

    Explored the concept of App Router

--- 024-2-NextJS/onwards-foodies-starting-project/app/meals/page.js
+++ 024-2-NextJS/onwards-foodies-starting-project/app/meals/page.js
@@ -5,6 +5,11 @@ import classes from "./page.module.css";
 import MealsGrid from "@/component/meals/meals-grid.js";
 import { getMeals } from "@/lib/meals";
 
+export const metadata = {
+  title: "All Meals",
+  description: "Browse the delicious meal shared by our vibrant community.",
+};
+
 async function Meals() {
   const meals = await getMeals();
   return <MealsGrid meals={meals} />;
