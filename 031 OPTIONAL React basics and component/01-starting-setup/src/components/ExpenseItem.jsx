30a846cd660309b0847213f7b1310db792f13675
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-12-30 04:43:08 +0100

    Cmpnents and props

--- 031 OPTIONAL React basics and component/01-starting-setup/src/components/ExpenseItem.jsx
+++ 031 OPTIONAL React basics and component/01-starting-setup/src/components/ExpenseItem.jsx
@@ -1,13 +1,40 @@
 import "./ExpenseItem.css";
 
-export default function ExpenseItem() {
+export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>March 28th, 2021</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
 }
+
+// .expense-date {
+//     display: flex;
+//     flex-direction: column;
+//     width: 5.5rem;
+//     height: 5.5rem;
+//     border: 1px solid #ececec;
+//     background-color: #2a2a2a;
+//     color: white;
+//     border-radius: 12px;
+//     align-items: center;
+//     justify-content: center;
+//   }
+
+//   .expense-date__month {
+//     font-size: 0.75rem;
+//     font-weight: bold;
+//   }
+
+//   .expense-date__year {
+//     font-size: 0.75rem;
+//   }
+
+//   .expense-date__day {
+//     font-size: 1.5rem;
+//     font-weight: bold;
+//   }
