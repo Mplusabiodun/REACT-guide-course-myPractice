3e33c1573137ae5ad1861d5170bf0b05c0a45c16
Fatai Abiodun M. <fataimonsuru2020@gmail.com>
2024-12-31 21:53:12 +0100

    splitting components

--- 031 OPTIONAL React basics and component/01-starting-setup/src/components/ExpenseItem.jsx
+++ 031 OPTIONAL React basics and component/01-starting-setup/src/components/ExpenseItem.jsx
@@ -1,9 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 export default function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
@@ -11,30 +12,3 @@ export default function ExpenseItem(props) {
     </div>
   );
 }
-
-// .expense-date {
-//     display: flex;
-//     flex-direction: column;
-//     width: 5.5rem;
-//     height: 5.5rem;
-//     border: 1px solid #ececec;
-//     background-color: #2a2a2a;
-//     color: white;
-//     border-radius: 12px;
-//     align-items: center;
-//     justify-content: center;
-//   }
-
-//   .expense-date__month {
-//     font-size: 0.75rem;
-//     font-weight: bold;
-//   }
-
-//   .expense-date__year {
-//     font-size: 0.75rem;
-//   }
-
-//   .expense-date__day {
-//     font-size: 1.5rem;
-//     font-weight: bold;
-//   }
